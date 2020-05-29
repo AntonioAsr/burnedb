f276bb2f60ee2ca3f60ded8394892c6d0070debf
Antonio Segovia <antonio.segovia@iris.net>
2020-05-29 18:34:09 +0100

    fix logo

---
+++ src/core/components/Burntbutter.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Logo from "../images/Logo.svg";
+
+const Burntbutter = () => {
+    return (
+        <div style={{ width: "200px", height: "75px", margin: "auto" }}>
+            <img src={Logo} alt="app logo"/>
+        </div>
+    );
+};
+export default Burntbutter;
