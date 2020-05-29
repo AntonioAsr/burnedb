67fd6d5990f27079feb6d69ffdbf9692ff84eaf3
Antonio Segovia <antonio.segovia@iris.net>
2020-05-29 18:34:47 +0100

    add service to create users

---
+++ src/core/data/services/createUser.js
@@ -0,0 +1,16 @@
+import { url } from "./endpoints.js";
+import axios from "axios";
+
+let createUser = async (userDetails) => {
+    try {
+        const createUserEndpoint = `${url.baseUrl}${url.createUser}`;
+        const response = await axios.post(`${createUserEndpoint}`, userDetails);
+        const data = await response.json();
+        return data;
+    }
+    catch (error) {
+        console.warn(error);
+    }
+};
+
+export default createUser;
