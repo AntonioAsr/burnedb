3938ba65a2d2c50424c8e0dbe0b65cb08f7d66c0
Antonio Segovia <antonio.segovia@iris.net>
2020-06-10 12:15:32 +0100

    remove innecesary return statement

--- src/core/data/services/createUser.js
+++ src/core/data/services/createUser.js
@@ -4,9 +4,7 @@ import axios from "axios";
 let createUser = async (userDetails) => {
     try {
         const createUserEndpoint = `${url.baseUrl}${url.createUser}`;
-        const response = await axios.post(`${createUserEndpoint}`, userDetails);
-        const data = await response.json();
-        return data;
+        await axios.post(`${createUserEndpoint}`, userDetails);
     }
     catch (error) {
         console.warn(error);
