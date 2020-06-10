0891dacbdb8dff21682fbd302320c7b73afade2a
Antonio Segovia <antonio.segovia@iris.net>
2020-06-10 12:14:15 +0100

    added header as styled component

---
+++ src/core/components/BurntButterHeader.js
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+import { COLORS } from "../constants";
+
+// import mediaQueries from "./mediaQueries";
+
+export const BurntButterHeader = styled.div`
+    position: "fixed";
+    top: 0px;
+    width: 100%;
+    height: 128px;
+    border-bottom: 1px solid #979797;
+    align-content: center;
+
+    
+    @media (min-width: 1200px) {
+
+    }
+
+    /* Landscape tablets and medium desktops */
+    @media (min-width: 992px) and (max-width: 1199px) {
+
+    }
+
+    /* Portrait tablets and small desktops */
+    @media (min-width: 768px) and (max-width: 991px) {
+
+    }
+
+
+    /* Portrait phones and smaller */
+    @media (max-width: 480px) {
+
+    }
+
+
+`;
