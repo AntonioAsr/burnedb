9a13c839f26bf6e6829593545a0f6d341644b2cc
Antonio Segovia <antonio.segovia@iris.net>
2020-07-13 22:20:34 +0100

    remove hardcoded get user  details by id and moved it to login service

--- src/core/data/services/loginUser.js
+++ src/core/data/services/loginUser.js
@@ -2,6 +2,9 @@ import { url } from "../endpoints.js";
 import axios from "axios";
 import store from "../combineReducers";
 import { logIn } from "../actions/userActions";
+import getUserById from "./getUserById.js";
+import { setUserDetails } from "../actions/userActions";
+
 
 const logInUser = async (userLogInDetails) => {
     try {
@@ -10,6 +13,11 @@ const logInUser = async (userLogInDetails) => {
         const data = await response;
         if (data.status === 200) {
             store.dispatch(logIn(response.data.userId));
+            getUserById(response.data.userId)
+            .then(res => {
+                store.dispatch(setUserDetails(res.data));
+            });
+
         }
         // todo change to asyn actions with redux-thunks
         return data;
